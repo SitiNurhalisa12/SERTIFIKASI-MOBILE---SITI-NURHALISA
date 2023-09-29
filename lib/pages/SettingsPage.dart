1d3adece97bc03b03f48489724b6f234724e8f5e
SitiNurhalisa12 <hlzlisa12@gmail.com>
2023-09-29 20:40:17 +0700

    first commit

---
+++ lib/pages/SettingsPage.dart
@@ -0,0 +1,129 @@
+import 'package:halizaapp/helper/dbhelper.dart';
+import 'package:halizaapp/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:halizaapp/providers/user_provider.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  TextEditingController currentPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
+
+  String developerName = "Siti Nurhalisa";
+  String developerNim = "2141764078";
+  String dateApp = "27 September 2023";
+
+  final DbHelper dbHelper = DbHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    // Access the UserProvider to get user data
+    final userProvider = Provider.of<UserProvider>(context);
+    final user = userProvider.user;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: const Text(
+                "Ubah Password",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            TextField(
+              controller: currentPasswordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password Saat Ini",
+                labelStyle: TextStyle(color: Colors.blue),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue)),
+              ),
+            ),
+            TextField(
+              controller: newPasswordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password Baru",
+                labelStyle: TextStyle(color: Colors.blue),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue)),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _changePassword(user!);
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.green.shade400),
+              ),
+              child: const Text("Simpan Password Baru"),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // Kembali
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.lightBlue.shade800)),
+                child: const Text("<< Kembali")),
+            const SizedBox(height: 100),
+            Row(
+              children: [
+                const CircleAvatar(
+                  backgroundImage: AssetImage(
+                      'assets/images/profile.jpg'),
+                  radius: 50,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // create bold heading style
+                      const Text(
+                        "Tentang Aplikasi",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      const Text("Aplikasi ini dibuat oleh: "),
+                      Text("Nama    : $developerName"),
+                      Text("NIM       : $developerNim"),
+                      Text("Tanggal : $dateApp"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _changePassword(User user) {
+    String currentPasswordInput = currentPasswordController.text;
+    String newPasswordInput = newPasswordController.text;
+
+    if (currentPasswordInput == user.password) {
+      // Password saat ini benar, simpan password baru
+      dbHelper.changePassword(user.email!, newPasswordInput);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Password berhasil diubah."),
+      ));
+    } else {
+      // Password saat ini salah
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Password saat ini salah. Ubah password gagal."),
+      ));
+    }
+  }
+}
